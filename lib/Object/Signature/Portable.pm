3059d6c6ea4c18a8e611e97b0e82f8ec53160a4e
Robert Rothenberg <rrwo@cpan.org>
2019-02-17 19:14:23 +0000

    Switch to Dist::Zilla

--- lib/Object/Signature/Portable.pm
+++ lib/Object/Signature/Portable.pm
@@ -1,5 +1,7 @@
 package Object::Signature::Portable;
 
+# ABSTRACT: generate portable fingerprints of objects
+
 use v5.10;
 
 use strict;
@@ -10,33 +12,10 @@ use Crypt::Digest;
 use Exporter::Lite;
 use JSON::MaybeXS;
 
-use version 0.77; our $VERSION = version->declare('v0.1.6');
+our $VERSION = 'v0.2.0';
 
 our @EXPORT    = qw/ signature /;
 our @EXPORT_OK = @EXPORT;
-
-=head1 NAME
-
-Object::Signature::Portable - generate portable fingerprints of objects
-
-=begin readme
-
-=head1 REQUIREMENTS
-
-This module requires Perl v5.10 or newer, and the following non-core
-modules:
-
-=over
-
-=item L<Crypt::Digest>
-
-=item L<Exporter::Lite>
-
-=item L<JSON::MaybeXS>
-
-=back
-
-=end readme
 
 =head1 SYNOPSIS
 
@@ -298,58 +277,12 @@ signatures.
 
 =back
 
-=head1 AUTHOR
-
-Robert Rothenberg C<< <rrwo@cpan.org> >> (on behalf of Foxtons, Ltd.)
+=head1 append:AUTHOR
 
 =head2 Acknowledgements
 
 Thanks to various people at YAPC::EU 2014 for suggestions about
 L<Sereal::Encoder>.
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013-2014 Robert Rothenberg.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-=for readme stop
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-=for readme continue
 
 =cut
 
